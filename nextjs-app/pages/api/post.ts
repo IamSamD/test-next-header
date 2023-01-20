d8deccfaab2bf631ae5ef73bdd27738cedf80ce0
sddev12 <sd.dev@outlook.com>
2023-01-20 00:03:28 +0000

    added nextjs-app

---
+++ nextjs-app/pages/api/post.ts
@@ -0,0 +1,12 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  cookie: string
+}
+
+export default function handler( req: NextApiRequest, res: NextApiResponse<Data> ) {
+  if (req.method == "POST") {
+    res.json({ cookie:`${req.headers.cookie}` })
+  }
+}
